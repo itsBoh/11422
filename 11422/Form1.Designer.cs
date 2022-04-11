26cfb582cbbd8760218d1bcf97d8deb98438127f
Alvin Wilbert <awilbert@student.ciputra.ac.id>
2022-04-11 09:38:02 +0700

    Add project files.

---
+++ 11422/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace _11422
+{
+    partial class lbCaptainHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbHome = new System.Windows.Forms.ComboBox();
+            this.cbAway = new System.Windows.Forms.ComboBox();
+            this.lbVs = new System.Windows.Forms.Label();
+            this.lbManagerHome = new System.Windows.Forms.Label();
+            this.lbManagerAway = new System.Windows.Forms.Label();
+            this.lbCaptHome = new System.Windows.Forms.Label();
+            this.lbCaptainAway = new System.Windows.Forms.Label();
+            this.lbStadium = new System.Windows.Forms.Label();
+            this.lbCapacity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbHome
+            // 
+            this.cbHome.FormattingEnabled = true;
+            this.cbHome.Location = new System.Drawing.Point(54, 29);
+            this.cbHome.Name = "cbHome";
+            this.cbHome.Size = new System.Drawing.Size(160, 21);
+            this.cbHome.TabIndex = 0;
+            this.cbHome.SelectedIndexChanged += new System.EventHandler(this.cbHome_SelectedIndexChanged);
+            // 
+            // cbAway
+            // 
+            this.cbAway.FormattingEnabled = true;
+            this.cbAway.Location = new System.Drawing.Point(489, 29);
+            this.cbAway.Name = "cbAway";
+            this.cbAway.Size = new System.Drawing.Size(139, 21);
+            this.cbAway.TabIndex = 1;
+            this.cbAway.SelectedIndexChanged += new System.EventHandler(this.cbAway_SelectedIndexChanged);
+            // 
+            // lbVs
+            // 
+            this.lbVs.AutoSize = true;
+            this.lbVs.Location = new System.Drawing.Point(331, 36);
+            this.lbVs.Name = "lbVs";
+            this.lbVs.Size = new System.Drawing.Size(18, 13);
+            this.lbVs.TabIndex = 2;
+            this.lbVs.Text = "vs";
+            // 
+            // lbManagerHome
+            // 
+            this.lbManagerHome.AutoSize = true;
+            this.lbManagerHome.Location = new System.Drawing.Point(54, 57);
+            this.lbManagerHome.Name = "lbManagerHome";
+            this.lbManagerHome.Size = new System.Drawing.Size(55, 13);
+            this.lbManagerHome.TabIndex = 3;
+            this.lbManagerHome.Text = "Manager :";
+            // 
+            // lbManagerAway
+            // 
+            this.lbManagerAway.AutoSize = true;
+            this.lbManagerAway.Location = new System.Drawing.Point(489, 57);
+            this.lbManagerAway.Name = "lbManagerAway";
+            this.lbManagerAway.Size = new System.Drawing.Size(55, 13);
+            this.lbManagerAway.TabIndex = 4;
+            this.lbManagerAway.Text = "Manager :";
+            // 
+            // lbCaptHome
+            // 
+            this.lbCaptHome.AutoSize = true;
+            this.lbCaptHome.Location = new System.Drawing.Point(54, 73);
+            this.lbCaptHome.Name = "lbCaptHome";
+            this.lbCaptHome.Size = new System.Drawing.Size(52, 13);
+            this.lbCaptHome.TabIndex = 5;
+            this.lbCaptHome.Text = "Captain : ";
+            // 
+            // lbCaptainAway
+            // 
+            this.lbCaptainAway.AutoSize = true;
+            this.lbCaptainAway.Location = new System.Drawing.Point(489, 73);
+            this.lbCaptainAway.Name = "lbCaptainAway";
+            this.lbCaptainAway.Size = new System.Drawing.Size(49, 13);
+            this.lbCaptainAway.TabIndex = 6;
+            this.lbCaptainAway.Text = "Captain :";
+            // 
+            // lbStadium
+            // 
+            this.lbStadium.AutoSize = true;
+            this.lbStadium.Location = new System.Drawing.Point(309, 106);
+            this.lbStadium.Name = "lbStadium";
+            this.lbStadium.Size = new System.Drawing.Size(45, 13);
+            this.lbStadium.TabIndex = 7;
+            this.lbStadium.Text = "Stadium";
+            // 
+            // lbCapacity
+            // 
+            this.lbCapacity.AutoSize = true;
+            this.lbCapacity.Location = new System.Drawing.Point(309, 119);
+            this.lbCapacity.Name = "lbCapacity";
+            this.lbCapacity.Size = new System.Drawing.Size(57, 13);
+            this.lbCapacity.TabIndex = 8;
+            this.lbCapacity.Text = "Capacity : ";
+            // 
+            // lbCaptainHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbCapacity);
+            this.Controls.Add(this.lbStadium);
+            this.Controls.Add(this.lbCaptainAway);
+            this.Controls.Add(this.lbCaptHome);
+            this.Controls.Add(this.lbManagerAway);
+            this.Controls.Add(this.lbManagerHome);
+            this.Controls.Add(this.lbVs);
+            this.Controls.Add(this.cbAway);
+            this.Controls.Add(this.cbHome);
+            this.Name = "lbCaptainHome";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormPraktikum_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbHome;
+        private System.Windows.Forms.ComboBox cbAway;
+        private System.Windows.Forms.Label lbVs;
+        private System.Windows.Forms.Label lbManagerHome;
+        private System.Windows.Forms.Label lbManagerAway;
+        private System.Windows.Forms.Label lbCaptHome;
+        private System.Windows.Forms.Label lbCaptainAway;
+        private System.Windows.Forms.Label lbStadium;
+        private System.Windows.Forms.Label lbCapacity;
+    }
+}
+
---
+++ 11422/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _11422
+{
+    public partial class lbCaptainHome : Form
+    {
+        public lbCaptainHome()
+        {
+            InitializeComponent();
+        }
+        public static string sqlconnection = "server=localhost;uid=root;pwd=;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlconnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqladapter;
+        public string sqlQuery;
+        string HomeManager;
+        string AwayManager;
+        string HomeCapt;
+        string AwayCapt;
+        string Stadium;
+        string Capacity;
+
+        private void FormPraktikum_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "SELECT team_name FROM team";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sqladapter.Fill(ds, "USE_team");
+            int size = ds.Tables[0].Rows.Count;
+            string[] namaTeam = new string[size];
+            int i = 0;
+            foreach(DataRow dr in ds.Tables[0].Rows)
+            {
+                namaTeam[i] = dr["team_name"].ToString();
+                i++;
+            }
+            cbAway.Items.AddRange(namaTeam);
+            cbHome.Items.AddRange(namaTeam);
+
+            DataTable dataTable = new DataTable();
+            sqlQuery = "SELECT t.team_name FROM team t";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dataTable);
+        }
+
+        private void cbHome_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string getTeam = cbHome.SelectedItem.ToString();
+            DataTable dtManagerHome = new DataTable();
+            sqlQuery = "SELECT manager_name FROM manager m LEFT JOIN team t ON m.manager_id = t.manager_id WHERE t.team_name = '" + getTeam + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtManagerHome);
+            HomeManager = dtManagerHome.Rows[0][0].ToString();
+
+            DataTable dtCaptHome = new DataTable();
+            sqlQuery = "SELECT p.player_name FROM player p LEFT JOIN team t ON t.team_id = p.team_id WHERE t.team_name = '" + getTeam + "' AND t.captain_id = p.player_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtCaptHome);
+            HomeCapt = dtCaptHome.Rows[0][0].ToString();
+
+            DataTable dtStadium = new DataTable();
+            sqlQuery = "SELECT home_stadium FROM team WHERE team_name = '" + getTeam + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtStadium);
+            Stadium = dtStadium.Rows[0][0].ToString();
+
+            DataTable dtCapacity = new DataTable();
+            sqlQuery = "SELECT capacity FROM team WHERE team_name = '" + getTeam + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtCapacity);
+            Capacity = dtCapacity.Rows[0][0].ToString();
+
+            lbCaptHome.Text = "Captain : " + HomeCapt;
+            lbManagerHome.Text = "Manager : " + HomeManager;
+            lbStadium.Text = "Stadium : " + Stadium;
+            lbCapacity.Text = "Capacity : " + Capacity;
+        }
+
+        private void cbAway_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string getTeam2 = cbAway.SelectedItem.ToString();
+            DataTable dtManagerAway = new DataTable();
+            sqlQuery = "SELECT manager_name FROM manager m LEFT JOIN team t ON m.manager_id = t.manager_id WHERE t.team_name = '" + getTeam2 + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtManagerAway);
+            AwayManager = dtManagerAway.Rows[0][0].ToString();
+
+            DataTable dtCaptAway = new DataTable();
+            sqlQuery = "SELECT p.player_name FROM player p LEFT JOIN team t ON t.team_id = p.team_id WHERE t.team_name = '" + getTeam2 + "' AND t.captain_id = p.player_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqladapter = new MySqlDataAdapter(sqlCommand);
+            sqladapter.Fill(dtCaptAway);
+            AwayCapt = dtCaptAway.Rows[0][0].ToString();
+
+            lbCaptainAway.Text = "Captain : " + AwayCapt;
+            lbManagerAway.Text = "Manager : " + AwayManager;
+        }
+    }
+}
